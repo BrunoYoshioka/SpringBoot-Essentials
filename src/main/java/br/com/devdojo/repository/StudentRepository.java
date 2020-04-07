ec63d3afaf995a3b3e240e0bb4106bf6a395d827
Bruno Yuji Yoshioka <bruno.yuji1995@gmail.com>
2020-04-07 12:09:39 -0300

    Encontrar por nome do estudante

--- src/main/java/br/com/devdojo/repository/StudentRepository.java
+++ src/main/java/br/com/devdojo/repository/StudentRepository.java
@@ -7,5 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface StudentRepository extends CrudRepository<Student, Long> {
-    List<Student> findByName(String name);
+    //List<Student> findByName(String name); // Encontrar por nome = no banco
+    List<Student> findByNameIgnoreCaseContaining(String name); // Encontrar por nome em partes
 }
