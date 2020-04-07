46692cedb84a06eb77185c1686e7cba378362166
Bruno Yuji Yoshioka <bruno.yuji1995@gmail.com>
2020-04-06 21:04:01 -0300

    Iniciando as classes

---
+++ src/main/java/br/com/devdojo/endpoint/StudentEndPoint.java
@@ -0,0 +1,25 @@
+package br.com.devdojo.endpoint;
+
+import br.com.devdojo.model.Student;
+import br.com.devdojo.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+@RestController
+@RequestMapping("student")
+public class StudentEndPoint {
+    @Autowired
+    private DateUtil dateUtil;
+    @RequestMapping(method = RequestMethod.GET, path = "/list")
+    public List<Student> listAll(){
+        System.out.println("Foi impresso em: " + dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return asList(new Student("Deku"), new Student("Todoroki"));
+    }
+}
