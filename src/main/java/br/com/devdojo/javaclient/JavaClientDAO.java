973482a54a7b5000a6d9168bddb5e07fdf83244a
Bruno Yuji Yoshioka <bruno.yuji1995@gmail.com>
2020-04-12 00:20:58 -0300

    Cliente Spring com RestTemplate - POST

---
+++ src/main/java/br/com/devdojo/javaclient/JavaClientDAO.java
@@ -0,0 +1,42 @@
+package br.com.devdojo.javaclient;
+
+import br.com.devdojo.model.PageableResponse;
+import br.com.devdojo.model.Student;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+public class JavaClientDAO {
+    private RestTemplate restTemplate = new RestTemplateBuilder()
+            .rootUri("http://localhost:8080/v1/user/students")
+            .basicAuthentication("bruno", "devdojo").build();
+    private RestTemplate restTemplateAdmin = new RestTemplateBuilder()
+            .rootUri("http://localhost:8080/v1/admin/students")
+            .basicAuthentication("bruno", "devdojo").build();
+    //getForEntity() retorna o objeto encapsulado no ResponseEntity, esse possui umas informações a mais como reader por exemplo, incluindo a propria resposta que é o objeto que queremos
+    //getForObject() retorna diretamente o objeto puro do jeito que quer trabalhar
+    public Student findById(long id){
+        return restTemplate.getForObject("/{id}", Student.class, id);
+    }
+
+    public List<Student> listAll(){
+        ResponseEntity<PageableResponse<Student>> exchange = restTemplate.exchange("/", HttpMethod.GET, null,
+                new ParameterizedTypeReference<PageableResponse<Student>>() {});
+        return exchange.getBody().getContent();
+    }
+
+    public Student save(Student student){
+        ResponseEntity<Student> exchangePost = restTemplateAdmin.exchange("/",
+                HttpMethod.POST, new HttpEntity<>(student, createJSONHeader()), Student.class);
+        return exchangePost.getBody();
+    }
+
+    private static HttpHeaders createJSONHeader(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
+    }
+}
--- src/main/java/br/com/devdojo/javaclient/JavaSpringClientTest.java
+++ src/main/java/br/com/devdojo/javaclient/JavaSpringClientTest.java
@@ -4,43 +4,26 @@ import br.com.devdojo.model.PageableResponse;
 import br.com.devdojo.model.Student;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 public class JavaSpringClientTest {
     public static void main(String[] args) {
-        RestTemplate restTemplate = new RestTemplateBuilder()
-                .rootUri("http://localhost:8080/v1/user/students")
-                .basicAuthentication("bruno", "devdojo").build();
-        //getForEntity() retorna o objeto encapsulado no ResponseEntity, esse possui umas informações a mais como reader por exemplo, incluindo a propria resposta que é o objeto que queremos
-        //getForObject() retorna diretamente o objeto puro do jeito que quer trabalhar
-        Student student = restTemplate.getForObject("/{id}", Student.class, 1);
-        ResponseEntity<Student> forEntity = restTemplate.getForEntity("/{id}", Student.class, 1);
-        System.out.println("getForObject:");
-        System.out.println(student);
-        System.out.println("----------------------\n");
-        System.out.println("getForEntity:");
-        System.out.println(forEntity);
-        System.out.println(forEntity.getBody()); //utilizar o estudante dentro do ResponseEntity
-        /*System.out.println("----------------------\n");
-        System.out.println("lista de estudantes, usando getForObject:");
-        Student[] students = restTemplate.getForObject("/", Student[].class); // fazer chamada para metodo get q retorna a lista de estudantes, usando getForObject
-        System.out.println(students);
-        System.out.println("----------------------\n");
-        System.out.println("exchange que retorna o ResponseEntity:");
-        ResponseEntity<List<Student>> exchange = restTemplate.exchange("/", HttpMethod.GET, null,
-                new ParameterizedTypeReference<List<Student>>() {
-                });
-        System.out.println(exchange.getBody());*/
 
-        System.out.println("----------------------\n");
-        System.out.println("lista de estudantes por ordenação, usando getForObject:");
-        ResponseEntity<PageableResponse<Student>> exchange = restTemplate.exchange("/?sort=id,desc&sort=name,asc", HttpMethod.GET, null,
-                new ParameterizedTypeReference<PageableResponse<Student>>() {
-                });
-        System.out.println(exchange);
+        Student studentPost = new Student();
+        studentPost.setName("John Wick");
+        studentPost.setEmail("john.wick@pencil.com");
+        JavaClientDAO dao = new JavaClientDAO();
+        System.out.println("Encontrar por id:\n" + dao.findById(10));
+        System.out.println("-----------------------------");
+        System.out.println("Listar todos:\n" + dao.listAll());
+        System.out.println("-----------------------------");
+        System.out.println("Registrar Estudante:\n" + dao.save(studentPost));
+        System.out.println("-----------------------------");
+        List<Student> students = dao.listAll();
+        System.out.println("Contar Estudantes:\n" + students);
+        System.out.println("-----------------------------");
     }
 }
